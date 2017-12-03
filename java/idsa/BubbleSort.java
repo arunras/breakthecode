2383641c26dba138fa7e9b2a445ed0fbad18857c
Arun <prith000@citymail.cuny.edu>
2017-12-02 20:20:10 -0500

    add bubbleSort function

--- java/idsa/BubbleSort.java
+++ java/idsa/BubbleSort.java
@@ -9,19 +9,39 @@ public class BubbleSort {
     for (int i = 0; i < A.length-1; i++) {
       for (int j = 0; j < A.length-1-i; j++) {
         if (A[j] > A[j+1]) {
-          temp = A[j+1];
-          A[j+1] = A[j];
-          A[j] = temp;
+          swap(A, j, j+1);
         }
       } 
     }
     return A;
   }
 
+  public static int[] bubbleSort(int[] A) {
+    boolean isSorted = false;  
+    int lastUnsorted = A.length - 1;
+    while(!isSorted) {
+      isSorted = true;
+      for (int i = 0; i < lastUnsorted; i++) {
+        if (A[i] > A[i+1]) {
+          swap(A, i, i+1);
+          isSorted = false;
+        }
+      }
+      lastUnsorted--;
+    }
+    return A;
+  }
+
+  public static void swap (int[] array, int i, int j) {
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 
   public static void main (String[] args) {
     int[] A = {8, 5, 9, 2, 4, 6, 3, 7, 1};
-    System.out.println("Result: " + Arrays.toString(sortBubble(A)));  
+    System.out.println("Array: " + Arrays.toString(A));
+    System.out.println("Result:" + Arrays.toString(bubbleSort(A)));  
   }
 
 }
