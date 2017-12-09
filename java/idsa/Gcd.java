21e5704178e06d0772c4bbb567d35e2932ae406d
Arun <prith000@citymail.cuny.edu>
2017-12-08 22:11:29 -0500

    add Gcd

---
+++ java/idsa/Gcd.java
@@ -0,0 +1,14 @@
+package idsa;
+
+public class Gcd {
+  public static int gcd(int a, int b) {
+    if (b == 0) {
+      return a;
+    }
+    return gcd(b, a % b);
+  }
+
+  public static void main(String[] args) {
+    System.out.println("GCD: " + gcd(30, 10));
+  }
+}
