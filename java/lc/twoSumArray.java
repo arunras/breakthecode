ec148a4fcde143062390eb87cb71416ddbc409f7
Arun <prith000@citymail.cuny.edu>
2018-01-05 13:25:50 -0500

    update twoSumArray

--- java/lc/twoSumArray.java
+++ java/lc/twoSumArray.java
@@ -8,6 +8,7 @@ package lc;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Arrays;
 
 public class twoSumArray {
   public int[] twoSum (int[] nums, int target) {
@@ -21,5 +22,12 @@ public class twoSumArray {
       map.put(nums[i], i);
     }
     throw new IllegalArgumentException ("No two sum solution");
+  }
+
+  public static void main(String[] args) {
+    twoSumArray ts = new twoSumArray();
+    int[] a = new int[] {2, 7, 11, 15};
+    int target = 9;
+    System.out.println("Result : " + Arrays.toString(ts.twoSum(a, target)));  
   }
 }
