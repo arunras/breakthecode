0666eafb50347d1b12133b4f253175b81626fc47
Arun <prith000@citymail.cuny.edu>
2020-02-08 17:18:08 -0600

    InsertionSort

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,14 +1,14 @@
 import java.util.Arrays;
 
-import sorts.SelectionSort;
+import sorts.InsertionSort;
 
 public class Main {
 	
 	public static void main(String[] args) {
 
-    int[] data = {55, 99, 33, 11, 22, 88, 44, 77, 66, 00};
+    int[] data = {55, 33, 44, 77, 22, 88, 66, 99, 11, 00};
     System.out.println("Array: " + Arrays.toString(data));
-    System.out.println("Sorted: " + Arrays.toString(SelectionSort.selectionSort(data)));
+    System.out.println("Sorted: " + Arrays.toString(InsertionSort.sort1(data)));
 				
 	}
 
---
+++ java/src/main/java/sorts/InsertionSort.java
@@ -0,0 +1,18 @@
+package sorts;
+
+public class InsertionSort {
+
+  public static int[] sort(int[] data) {
+    for (int i=0; i<data.length-1; i++) {
+      for (int j=i+1; j>0; j--) {
+        if (data[j] < data[j-1]) {
+          int temp = data[j-1];
+          data[j-1] = data[j];
+          data[j] = temp;
+        }
+      }
+    }
+    return data;
+  }
+  
+}
