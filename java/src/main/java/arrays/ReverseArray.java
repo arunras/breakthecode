049510108984675d1e3bd5a7f621ba427862f206
Arun <prith000@citymail.cuny.edu>
2020-02-12 23:25:53 -0600

    ReverseArray

---
+++ java/src/main/java/arrays/ReverseArray.java
@@ -0,0 +1,37 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReverseArray {
+
+  public static int[] compute(int[] data) {
+   int array[] = new int[data.length];
+   int j=0;
+   for (int i = data.length - 1; i >= 0; i--) {
+     array[j] = data[i];
+     j++;
+   }
+   return array;
+  }
+
+  public static int[] swap(int[] data) {
+    for (int i = 0; i < data.length / 2; i++) {
+     int temp = data[data.length - i - 1];
+     data[data.length - i - 1] = data[i];
+     data[i] = temp; 
+    }
+    return data;
+  }
+
+  public static int[] listCollection(int[] data) {
+    List<Integer> list = Arrays.stream(data).boxed().collect(Collectors.toList());
+    Collections.reverse((list));
+    return list.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+  }
+
+}
