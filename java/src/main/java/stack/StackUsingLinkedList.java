d9c5b1cd176123da325e17fc8bc12c4c0c9ea1fd
Arun <prith000@citymail.cuny.edu>
2020-02-29 19:21:11 -0600

    StackUnsingLinkedList

---
+++ java/src/main/java/stack/StackUsingLinkedList.java
@@ -0,0 +1,34 @@
+package stack;
+
+public class StackUsingLinkedList {
+  private class Node {
+    int data;
+    Node next;
+  }
+
+  public Node head;
+
+  public void push(int data) {
+    Node node = new Node();
+    node.data = data;
+    node.next = head;
+    head = node;
+  }
+
+  public int pop() {
+    if (head != null) {
+      int data = head.data;
+      head = head.next;
+      return data;
+    }
+    return -1;
+  }
+
+  public void printList(Node head) {
+    Node node = head;
+    while (node != null) {
+      System.out.println(node.data);
+      node = node.next;
+    }
+  }
+}
