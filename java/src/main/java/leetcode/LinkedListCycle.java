b037bb1efaed94d72d866125a4eda2cbdd6cb74b
Arun <prith000@citymail.cuny.edu>
2020-03-07 18:32:51 -0600

    LinkedListCycle

---
+++ java/src/main/java/leetcode/LinkedListCycle.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class LinkedListCycle {
+
+  public boolean hasCycle(ListNode head) {
+    ListNode slow, fast;  
+    slow = fast = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if (slow == fast)
+        return true;
+    }
+    return false;
+  }
+
+}
