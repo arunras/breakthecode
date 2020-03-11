72a88f7ef58d4222ff36af80333efacfa6064b95
Arun <prith000@citymail.cuny.edu>
2020-03-11 13:39:43 -0500

    reverseLinkedListInPair

--- java/src/main/java/linkedlist/LinkedList.java
+++ java/src/main/java/linkedlist/LinkedList.java
@@ -235,6 +235,24 @@ public class LinkedList {
     }
     return null;
   }
+
+  public Node reverseLinkedListInPair(Node head) {
+    Node current = head;
+    Node temp = null;
+    Node newHead = null;
+    while (current != null && current.next != null) {
+      if (temp != null) 
+        temp.next.next = current.next;
+      temp = current.next;
+      current.next = temp.next;
+      temp.next = current;
+      
+      if (newHead == null)
+        newHead = temp;
+      current = current.next;
+    }
+    return newHead;
+  }
 //===algorithms==========================================================
 }
 
