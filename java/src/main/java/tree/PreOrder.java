8ef255d50204bb792c282fdc1467a3307871c5e4
Arun <prith000@citymail.cuny.edu>
2020-03-14 17:27:07 -0500

    tree

---
+++ java/src/main/java/tree/PreOrder.java
@@ -0,0 +1,32 @@
+package tree;
+
+import java.util.Stack;
+
+public class PreOrder {
+
+  public void preorderRecursive(TreeNode root) {
+    if (root != null) {
+      System.out.printf("%d ", root.data);
+      preorderRecursive(root.left);
+      preorderRecursive(root.right);
+    }
+  }
+
+  public void preorderIterative(TreeNode root) {
+    if (root == null)
+     return; 
+
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    stack.push(root); 
+    while (!stack.isEmpty()) {
+      TreeNode node = stack.pop();
+      System.out.printf("%d ", node.data);
+      
+      if (node.right != null)
+        stack.push(node.right);
+      if (node.left != null) 
+        stack.push(node.left);
+    }
+  }
+
+}
